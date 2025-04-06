723061d68ec69af3a9330633ccebeb99695a92c8
somto ogbodo <somtoogbodo2007@gmail.com>
2025-04-06 12:55:13 +0100

    working on the connection page

--- server/connection.php
+++ server/connection.php
@@ -2,18 +2,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
 function checkUrlProtocol($url)
 {
-    // Parse the URL to get the scheme
     $parsedUrl = parse_url($url);
-
-    // Check if the scheme exists and if it's http or https
-    if (isset($parsedUrl['scheme'])) {
-        return $parsedUrl['scheme'];
-    } else {
-        return 'invalid'; // Invalid URL if no scheme is found
-    }
+    return isset($parsedUrl['scheme']) ? $parsedUrl['scheme'] : 'invalid';
 }
 
 // Automatically get the current URL
@@ -26,33 +18,44 @@ $request = checkUrlProtocol($currentUrl);
 // Default configurations
 define("HOST", "localhost");
 
+// Determine if online or offline
+$isLocalhost = ($_SERVER['HTTP_HOST'] === 'localhost');
 
-// Set configurations based on protocol
-if ($request == 'https') {
-    $domain = "https://www.bodyandsoulrelax.com/";
-    define("USER", "pdicsrdj_massage_spa");
-    define("PASSWORD", "pdicsrdj_massage_spa");
-    define("DATABASE", "pdicsrdj_massage_spa");
-}
-elseif ($request == 'http') {
+// Database connection (Only use one based on environment)
+$connection = '';
+
+if ($isLocalhost) {
+    // Offline (Localhost)
     $domain = "http://localhost/Massage_spa/";
+
     define("USER", "root");
     define("PASSWORD", "");
     define("DATABASE", "message");
+
+    // Database connection
+    $connection = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+} else {
+    // Online (Live Server)
+    $domain = "https://www.bodyandsoulrelax.com/";
+
+    define("USER", "pdicsrdj_massage_spa");
+    define("PASSWORD", "pdicsrdj_massage_spa");
+    define("DATABASE", "pdicsrdj_massage_spa");
+
+    // Database connection
+    $connection = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
 }
 
-// // Database connection
-$connection = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
-
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-// // Site configurations
+// Site configurations
 $sitename = " Body And Soul Relax";
-
-
-// email config 
 $siteemail = "circleofbeauty@gmail.com";
 $emailpassword  = "support@bodyandsoulrelax.com";
 $site_email  = "support@bodyandsoulrelax.com";
@@ -60,11 +63,6 @@ $host = 'mail.bodyandsoulrelax.com';
 $sitephone  = "0744 3961 597";
 $sitephone2 = "0781 0556 515";
 $siteaddress  = "At Caversham Houses, 18 kingston Gardens Beddington Surrey Cro 4tx";
-
-
-session_start();
-
-
 
 
 // define('PAYPAL_CLIENT_ID', 'AawF0X5PzfwTzTdKq3Y25y2P49b8cnuGh01kkIPT_-QH0nMu4X511HnYgXbt9kbUTR1uoKrIYzGZ1r72');
@@ -97,8 +95,7 @@ function getAccessToken()
 }
 
 
+session_start();
 
 
-
-
-
+?>
